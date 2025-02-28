bd000e21782a662e797e1233673a46e032f833cc
Viktor Fougstedt <viktor@chalmers.se>
2025-02-28 15:10:58 +0100

    Cleanups.

--- top/toplevel.py
+++
@@ -1,8 +0,0 @@
-import dataclasses
-from top import *
-
-@dataclasses.dataclass(kw_only=True)
-class Message:
-    """Toppobjekt med enkla och listvärda referenser till samtliga värdeobjekt. Bra grund för meddelanden!"""
-    person: Person = None
-    persons: list[Person] = None
